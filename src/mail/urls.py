cc7fc09d5ff784840643e393c9498be5e2a8c994
sdfy-fly <rozhov25@bk.ru>
2023-05-18 20:22:13 +0300

    mailing stats

--- src/mail/urls.py
+++ src/mail/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import MailingView
+from .views import MailingView, MailingStatsView
 
 router = routers.SimpleRouter()
+router.register('mailing/stats', MailingStatsView, basename='mailing-stats')
 router.register('mailing', MailingView)
 
 urlpatterns = [
--- src/mail/views.py
+++ src/mail/views.py
@@ -1,11 +1,16 @@
+import http
+
+from django.db.models import Count
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from src.mail.models import Mailing
 from src.mail.serializer import MailingSerializer
+from src.message.models import Message
+from src.message.serializer import MessageSerializer
 
 
 class MailingView(ModelViewSet):
-
     queryset = Mailing.objects.all()
     serializer_class = MailingSerializer
 
@@ -22,3 +27,44 @@ class MailingView(ModelViewSet):
 
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
+
+
+class MailingStatsView(ModelViewSet):
+    queryset = Mailing.objects.all()
+    serializer_class = MailingSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        data = serializer.data
+
+        for item in data:
+            message_stats = Message.objects.filter(mailing_id=item['id']) \
+                .values('send_status').annotate(count=Count('id'))
+
+            item['message_stats'] = message_stats
+
+        return Response(data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+
+        # Получаем сообщения по рассылке
+        messages = Message.objects.filter(mailing=instance)
+        message_serializer = MessageSerializer(messages, many=True)
+
+        # Добавляем статистику в ответ
+        data = serializer.data
+        data['messages'] = message_serializer.data
+
+        return Response(data)
+
+    def create(self, request, *args, **kwargs):
+        return Response({"detail": "Method \"POST\" not allowed."}, http.HTTPStatus.METHOD_NOT_ALLOWED)
+
+    def update(self, request, *args, **kwargs):
+        return Response({"detail": "Method \"PUT\" not allowed."}, http.HTTPStatus.METHOD_NOT_ALLOWED)
+
+    def destroy(self, request, *args, **kwargs):
+        return Response({"detail": "Method \"DELETE\" not allowed."}, http.HTTPStatus.METHOD_NOT_ALLOWED)
