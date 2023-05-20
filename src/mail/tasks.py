87e1390aba3f62ec100b3ce056d1c1b908376889
sdfy-fly <rozhov25@bk.ru>
2023-05-20 12:34:54 +0300

    celery task

--- src/mail/tasks.py
+++ src/mail/tasks.py
@@ -1,3 +1,79 @@
+import httpx as httpx
+import logging
+
+from asgiref.sync import async_to_sync
 from celery import shared_task
 
+from datetime import datetime
+from django.db.models import Q
+from django.utils.timezone import make_aware
+
+from src.client.models import Client
+from src.mail.models import Mailing
+from src.message.models import Message
+
+logger = logging.getLogger('mailing_logger')
+logger.setLevel(logging.INFO)
+
+# Создание обработчика для записи логов в файл
+file_handler = logging.FileHandler('mailing.log')
+file_handler.setLevel(logging.INFO)
+
+# Форматирование сообщений логов
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# Добавление обработчика к логгеру
+logger.addHandler(file_handler)
+
+
+def get_current_time():
+    return make_aware(datetime.now())
+
+
+async def send_message(data):
+    url = f"https://probe.fbrq.cloud/v1/send/{data['id']}"
+    headers = {
+        'accept': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTU5MjczMjIsImlzcyI6ImZhYnJpcXVlIiwibmFtZSI6Imh0dHBzOi8vdC5tZS9zZGZ5bmFtZSJ9.98AOWUkw_EmRykQLfNgRwHO0awFmu6DR-MtrahIfYco',
+        'Content-Type': 'application/json'
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers, json=data)
+        return response
+
+
+@shared_task
+def process_mailing(mailing_id):
+    mailing = Mailing.objects.get(pk=mailing_id)
+
+    if not (mailing.start_time <= get_current_time() <= mailing.finish_time):
+        return
+
+    client_list = Client.objects.filter(
+        Q(operator_code=mailing.client_filter_operator_code) | Q(tag=mailing.client_filter_tag)
+    )
+
+    for client in client_list:
+
+        if get_current_time() > mailing.finish_time:
+            return
+
+        message = Message.objects.create(send_status='В работе', mailing=mailing, client=client)
+        data = {
+            "id": message.id,
+            "phone": message.client.phone_number,
+            "text": message.mailing.message_text
+        }
+
+        r = async_to_sync(send_message)(data)
+
+        if r.status_code == 200:
+            message.send_status = 'Отправлено'
+            logger.info(f"Mailing №{mailing.id} - Client №{client.id}: Письмо успешно отправлено")
+        else:
+            message.send_status = 'Не отправлено'
+            logger.error(f"Mailing №{mailing.id} - Client №{client.id}: Ошибка при отправке письма клиенту")
+
+        message.save()
 
--- src/mail/views.py
+++ src/mail/views.py
@@ -1,12 +1,15 @@
 import http
+from datetime import datetime
 
 from django.db.models import Count
+from django.utils.timezone import make_aware
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Mailing
 from .serializer import MailingSerializer
-from .tasks import hello
+from .tasks import process_mailing
+
 from src.message.models import Message
 from src.message.serializer import MessageSerializer
 
@@ -16,16 +19,38 @@ class MailingView(ModelViewSet):
     serializer_class = MailingSerializer
 
     def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
-        # TODO: асинхронно вызывать задачу celery
+        response = super().create(request, *args, **kwargs)
 
-    def retrieve(self, request, *args, **kwargs):
-        hello.delay()
-        return super().retrieve(request, *args, **kwargs)
+        mailing = Mailing.objects.get(id=response.data['id'])
+        self.__mailing(mailing)
+        return response
 
     def update(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
-        # TODO: асинхронно вызывать задачу celery
+        response = super().update(request, *args, **kwargs)
+
+        self.__mailing(self.get_object())
+        return response
+
+    @staticmethod
+    def __mailing(mailing):
+        """
+            Приватный метод, обертка для запуска celery задач
+            Вызывается после создания или обновления рассылки
+        """
+        current_time = make_aware(datetime.now())
+        if current_time >= mailing.finish_time:
+            return
+
+        # рассчитываю задержку перед началом задачи
+        # если она отрицательная, то запускаю задачу без задержки
+        delay = (mailing.start_time - current_time).total_seconds()
+        if delay < 0:
+            delay = 0
+
+        process_mailing.apply_async(args=[mailing.id], countdown=delay)
+
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
@@ -41,6 +66,7 @@ class MailingStatsView(ModelViewSet):
         data = serializer.data
 
         for item in data:
+            # Группируем сообщения, которые относятся к рассылке по статусу и считаем их количество
             message_stats = Message.objects.filter(mailing_id=item['id']) \
                 .values('send_status').annotate(count=Count('id'))
 
@@ -52,11 +78,9 @@ class MailingStatsView(ModelViewSet):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
 
-        # Получаем сообщения по рассылке
         messages = Message.objects.filter(mailing=instance)
         message_serializer = MessageSerializer(messages, many=True)
 
-        # Добавляем статистику в ответ
         data = serializer.data
         data['messages'] = message_serializer.data
 
